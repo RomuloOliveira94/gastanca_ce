37b7f883d6fa9bf01aa38131f932ffa8d17463f4
Rômulo Olveira <romulo.oliveira@cadena.com.br>
2025-05-16 10:11:05 -0300

    #add deputy tests and relations

--- app/models/deputy.rb
+++ app/models/deputy.rb
@@ -1,2 +1,4 @@
 class Deputy < ApplicationRecord
+  has_many :expenses, dependent: :destroy
+  validates_presence_of :name, :integration_id, :cpf, :state
 end
--- spec/models/deputy_spec.rb
+++ spec/models/deputy_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Deputy, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:integration_id) }
+    it { should validate_presence_of(:cpf) }
+    it { should validate_presence_of(:state) }
+  end
+
+  describe 'associations' do
+    it { should have_many(:expenses) }
+  end
+
+  describe 'database columns' do
+    it { should have_db_column(:name).of_type(:string) }
+    it { should have_db_column(:integration_id).of_type(:string) }
+    it { should have_db_column(:cpf).of_type(:string) }
+    it { should have_db_column(:state).of_type(:string) }
+  end
 end

fc1948d8407c97bffdcd84df4d2539004a1f75ef
Rômulo Olveira <romulo.oliveira@cadena.com.br>
2025-05-16 17:56:38 -0300

    #configure deputy url image

--- app/models/deputy.rb
+++ app/models/deputy.rb
@@ -2,4 +2,8 @@ class Deputy < ApplicationRecord
   has_many :expenses, dependent: :destroy
   belongs_to :party
   validates :name, :integration_id, :cpf, :state, presence: true
+
+  def image_url
+    "http://www.camara.leg.br/internet/deputado/bandep/#{integration_id}.jpg"
+  end
 end
--- app/views/api/v1/deputies/index.json.jbuilder
+++ app/views/api/v1/deputies/index.json.jbuilder
@@ -1,6 +1,7 @@
 json.array! @deputies do |deputy|
   json.id deputy.id
   json.name deputy.name
+  json.image deputy.image_url
   json.party deputy.party.acronym
   json.state deputy.state
 end
